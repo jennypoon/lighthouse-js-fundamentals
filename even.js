8cee87deee0beda564fb106148d725d3f97b8289
Jenny Poon <jenny.poon@live.ca>
2018-08-27 05:06:37 +0000

    Included Odd checker function to exercise

--- even.js
+++ even.js
@@ -15,4 +15,9 @@ function isEven(num) {
 }
 
 console.log(isEven(10));
-console.log(isEven(11));
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
